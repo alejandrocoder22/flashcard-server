d2fc2d0267acabc47a8220434bd573387a7b2fa7
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-10 14:27:44 +0200

    Added constraint to user and email

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
-    userId SERIAL,
+    userId SERIAL unique,
     password VARCHAR(100),
-    email VARCHAR(45),
+    email VARCHAR(45) unique,
     created_on TIMESTAMP DEFAULT now()
 );
