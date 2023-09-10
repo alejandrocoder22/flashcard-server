3909abaf773182f252b1bd36bdddf53c87387746
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-10 15:50:41 +0200

    Added primary key

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,9 +1,10 @@
 CREATE TABLE user (
-    userId SERIAl,
+    userd_id SERIAl,
     username VARCHAR(15) NOT NULL unique,
     password VARCHAR(100) NOT NULL,
     email VARCHAR(45) NOT NULL unique,
     created_on TIMESTAMP NOT NULL DEFAULT now()
+    PRIMARY KEY(user_id)
 );
 
 CREATE TABLE card (
