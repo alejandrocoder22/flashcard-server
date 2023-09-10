065088f737e7e979d0f44f983508458c492c1564
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-10 15:40:08 +0200

    Added more constraints to user

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,6 +1,11 @@
-CREATE TABLE users (
-    userId SERIAL unique,
-    password VARCHAR(100),
-    email VARCHAR(45) unique,
-    created_on TIMESTAMP DEFAULT now()
+CREATE TABLE user (
+    userId SERIAl,
+    username VARCHAR(15) NOT NULL unique,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(45) NOT NULL unique,
+    created_on TIMESTAMP NOT NULL DEFAULT now()
 );
+
+CREATE TABLE card (
+    cardId SERIAL,
+)
