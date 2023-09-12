b67a0a6e83a94084e51b82d5ed14024597892234
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-12 19:51:39 +0200

    Added card router

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import usersRouter from './router/usersRouter'
+import cardRouter from './router/cardsRouter'
+
 const app = express()
 
 app.use('/api/users', usersRouter)
+app.use('/api/cards', cardRouter)
 
 app.listen(3001, () => {
   console.log('Listening on port 3001')
