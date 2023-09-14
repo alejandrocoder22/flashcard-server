34a73e3887c69db964bf2256801e54549afa21a5
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-14 09:37:08 +0200

    Moved dontenv config to the top

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
+import 'dotenv/config'
 import express from 'express'
 import usersRouter from './router/usersRouter'
 import cardRouter from './router/cardsRouter'
-import 'dotenv/config'
 
 const app = express()
 
 app.use('/api/users', usersRouter)
 app.use('/api/cards', cardRouter)
-
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`)
 })
