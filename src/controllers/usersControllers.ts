06f547a22aa09b0435a4bb04b8a8b3b8795a740d
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-15 09:47:11 +0200

    Linter

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -2,7 +2,7 @@
 import express from 'express'
 import { pool } from '../database/database'
 
-export const createUser = async (_req: any, res: express.Response) => {
+export const createUser: any = async (_req: any, res: express.Response) => {
   const cards = await pool.query('SELECT * FROM cards')
 
   res.status(200).send(cards)
