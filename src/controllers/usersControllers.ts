5aa222fae95289bc561f31e2c1cedb1de7a9f4da
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-19 09:53:34 +0200

    Added logic to prevent query if user already exist

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,11 +1,23 @@
 
 import express from 'express'
 import { pool } from '../database/database'
+import { userExist } from '../services/usersServices'
 
-export const createUser: any = async (_req: any, res: express.Response) => {
-  const cards = await pool.query('SELECT * FROM cards')
+export const createUser: any = async (req: express.Request, res: express.Response) => {
+  const { username, email } = req.body
 
-  res.status(200).send(cards)
+  try {
+    const user = await userExist(username, email)
+    if (user.length > 0) {
+      res.status(200).send({message: 'User or email already exist'})
+    }
+
+    
+
+    res.status(200).send({ message: 'User created' })
+  } catch (error) {
+    res.status(400).send('error')
+  }
 }
 
 export const loginUser = (_req: express.Request, res: express.Response) => {
