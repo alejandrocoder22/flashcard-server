49808bb7cf0ab86fe6ab9a8011b6645f25350714
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-21 09:32:51 +0200

    Added hash logic to createUser Controller

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,21 +1,20 @@
 
 import express from 'express'
-import { userExist } from '../services/usersServices'
+import { userExist, createUserService } from '../services/usersServices'
 import { hashPassword } from '../utils/crypto'
 
 export const createUser: any = async (req: express.Request, res: express.Response) => {
   const { username, email, password } = req.body
-
   try {
     const user = await userExist(username, email)
     if (user.length > 0) {
-      res.status(200).send({ message: 'User or email already exist', password: hashedPassword })
+      return res.status(400).send({ message: 'User or email already exist' })
     }
     const hashedPassword = await hashPassword(password)
-    createUser(username, hashedPassword, email)
-    res.status(200).send({ message: 'User created' })
+    const createdUser: any = await createUserService(username, email, hashedPassword)
+    if (createdUser.rows.length > 0) res.status(200).send({ message: 'User created' })
   } catch (error) {
-    res.status(400).send('error')
+    res.status(400).send(error)
   }
 }
 
