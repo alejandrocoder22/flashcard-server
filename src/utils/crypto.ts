a2a93e4163e181371a460dab33394c7a249881da
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-21 09:57:11 +0200

    Added generateToken function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-
+import jwt from 'jsonwebtoken'
 export const hashPassword = async (password: string) => {
   const saltRounds = 10
   const hashedPassword = await bcrypt.hash(password, saltRounds)
@@ -9,4 +9,9 @@ export const hashPassword = async (password: string) => {
 export const checkPassword = async (plainPassword: string, hashedPassword: string) => {
   const isPasswordRight = await bcrypt.compare(plainPassword, hashedPassword)
   return isPasswordRight
+}
+
+export const generateToken =  async (userId: number, username: string) => {
+  const userData = { userId, username }
+  return jwt.sign(userData, String(process.env.JWT_PASSWORD))
 }
