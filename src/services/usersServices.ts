961c391204ba767418881cf50f7cd07afa2465de
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-21 09:13:26 +0200

    Added createUser service

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -4,3 +4,7 @@ export const userExist = async (username: string, email: string) => {
   const user = await pool.query('SELECT * FROM users WHERE username = $1 OR email = $2', [username, email])
   return user.rows
 }
+
+export const createUser = (username: string, email: string, hashedPassword: string) => {
+  return pool.query('INSERT INTO users (username, password, email) VALUES($1, $2, $3)', username, hashedPassword, email)
+}
