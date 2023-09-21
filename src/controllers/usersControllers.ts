4e030a6760d30792899ccc78a1dd4eb48bbb1bbc
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-21 09:13:34 +0200

    Create user controller

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,19 +1,18 @@
 
 import express from 'express'
-import { pool } from '../database/database'
 import { userExist } from '../services/usersServices'
+import { hashPassword } from '../utils/crypto'
 
 export const createUser: any = async (req: express.Request, res: express.Response) => {
-  const { username, email } = req.body
+  const { username, email, password } = req.body
 
   try {
     const user = await userExist(username, email)
     if (user.length > 0) {
-      res.status(200).send({message: 'User or email already exist'})
+      res.status(200).send({ message: 'User or email already exist', password: hashedPassword })
     }
-
-    
-
+    const hashedPassword = await hashPassword(password)
+    createUser(username, hashedPassword, email)
     res.status(200).send({ message: 'User created' })
   } catch (error) {
     res.status(400).send('error')
