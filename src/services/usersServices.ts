791bdc03aa3ebcd6cc4106710191da5bb2d9c8c0
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-21 09:50:43 +0200

    Added defailt empty email

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -1,6 +1,6 @@
 import { pool } from '../database/database'
 
-export const userExist = async (username: string, email: string) => {
+export const userExist = async (username: string, email: string = '') => {
   const user = await pool.query('SELECT * FROM users WHERE username = $1 OR email = $2', [username, email])
   return user.rows
 }
