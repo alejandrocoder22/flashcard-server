d03987a05cec582b71b6f5352575fd1a1e609765
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-24 09:08:22 +0200

    Add token response in generateToken

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import express from 'express'
 export const hashPassword = async (password: string) => {
   const saltRounds = 10
   const hashedPassword = await bcrypt.hash(password, saltRounds)
@@ -11,7 +12,14 @@ export const checkPassword = async (plainPassword: string, hashedPassword: strin
   return isPasswordRight
 }
 
-export const generateToken = async (userId: number, username: string) => {
+export const generateToken = async (userId: number, username: string, res: express.Resposne) => {
   const userData = { userId, username }
-  return jwt.sign(userData, String(process.env.JWT_PASSWORD))
+
+  const token = await jwt.sign(userData, String(process.env.JWT_PASSWORD))
+
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  })
 }
