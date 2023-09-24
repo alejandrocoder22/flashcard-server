80583bbf63e231df98af9a78adf6ece42bfa9fb1
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-24 09:16:45 +0200

    Added type to flashCard argument

--- src/services/cardsServices.ts
+++ src/services/cardsServices.ts
@@ -1,4 +1,5 @@
 import { pool } from '../database/database'
+import { flashCard } from '../types'
 
 export const getAllPublicCards = async () => {
   return await pool.query('SELECT * FROM cards WHERE is_public = true')
@@ -8,6 +9,6 @@ export const getCardsById = async (userId: number) => {
   return await pool.query('SELECT * FROM cards WHERE user_id = $1', [userId])
 }
 
-export const createCard = async (cardData) => {
+export const createCard = async (cardData: flashCard) => {
   return await pool.query('INSERT INTO cards (topic, question, answer, user_id, is_public) VALUES ($1, $2, $3, $4, $5)', [cardData.topic, cardData.question, cardData.answer, cardData.user_id, cardData.is_public])
 }
