25d5c54da4438467d19e3fe9235825aebf02732a
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-24 09:43:10 +0200

    Add coopy parser to middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import 'dotenv/config'
 import express from 'express'
 import usersRouter from './router/usersRouter'
 import cardRouter from './router/cardsRouter'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/users', usersRouter)
 app.use('/api/cards', cardRouter)
