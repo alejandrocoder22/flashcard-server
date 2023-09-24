378129c2caddafdf53721892c900f9812fe755e9
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-24 09:16:51 +0200

    Added optional types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,10 @@
 export interface flashCard {
-  card_id: number
+  card_id?: number
   topic: string
   question: string
   answer: string
-  user_id: number
+  user_id?: number
   is_public: boolean
-  created_at: string
-  user_id: number
+  created_at?: string
+  user_id?: number
 }
