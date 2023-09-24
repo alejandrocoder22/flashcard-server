3011bfbde9867386e1dd5b874f1f4849ac6af6c0
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-24 09:15:13 +0200

    Added flashCard types

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export interface flashCard {
+  card_id: number
+  topic: string
+  question: string
+  answer: string
+  user_id: number
+  is_public: boolean
+  created_at: string
+  user_id: number
+}
