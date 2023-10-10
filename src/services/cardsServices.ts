bab7313ef3f3c280a723c490386a649c8d429b8e
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-10-10 20:09:42 +0200

    Added return type to functions

--- src/services/cardsServices.ts
+++ src/services/cardsServices.ts
@@ -1,14 +1,14 @@
 import { pool } from '../database/database'
 import { flashCard } from '../types'
 
-export const getAllPublicCards = async () => {
+export const getAllPublicCards = async (): Promise<any> => {
   return await pool.query('SELECT * FROM cards WHERE is_public = true')
 }
 
-export const getCardsById = async (userId: number) => {
+export const getCardsById = async (userId: number): Promise<any> => {
   return await pool.query('SELECT * FROM cards WHERE user_id = $1', [userId])
 }
 
-export const createCard = async (cardData: flashCard) => {
+export const createCard = async (cardData: flashCard): Promise<any> => {
   return await pool.query('INSERT INTO cards (topic, question, answer, user_id, is_public) VALUES ($1, $2, $3, $4, $5)', [cardData.topic, cardData.question, cardData.answer, cardData.user_id, cardData.is_public])
 }
