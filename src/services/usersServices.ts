117789c057fc470aea05ed3d9452ecb13f0c2b04
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-10-19 17:56:14 +0200

    Added deleteUser service

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -8,3 +8,7 @@ export const userExist = async (username: string, email: string = '') => {
 export const createUserService = async (username: string, email: string, hashedPassword: string) => {
   return await pool.query('INSERT INTO users (username, password, email) VALUES($1, $2, $3) RETURNING *', [username, hashedPassword, email])
 }
+
+export const deleteUserService = async (userId: number) => {
+  return await pool.query('DELETE FROM users WHERE user_id = $1', [userId])
+}
