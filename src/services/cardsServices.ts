9078822db7cad2b307801a945e4483fb13df4973
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-21 06:59:40 +0100

    Added delete query

--- src/services/cardsServices.ts
+++ src/services/cardsServices.ts
@@ -12,3 +12,7 @@ export const getCardsById = async (userId: number): Promise<any> => {
 export const createCard = async (cardData: flashCard): Promise<any> => {
   return await pool.query('INSERT INTO cards (topic, question, answer, user_id, is_public) VALUES ($1, $2, $3, $4, $5)', [cardData.topic, cardData.question, cardData.answer, cardData.user_id, cardData.is_public])
 }
+
+export const deleteCard = async (userId, cardId) => {
+  return await pool.query('DELETE FROM cards WHERE card_id = $1', [cardId])
+}
