2d260bb1cc3469abb4b5e8fa2701e2ad97fbdec4
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-26 11:50:05 +0100

    Added createDeck Controller

--- src/controllers/decksControllers.ts
+++ src/controllers/decksControllers.ts
@@ -4,6 +4,15 @@
 // Delete Deck
 // Update Deck
 
-const getDecksByTopic = (req, res) => {
-  c
+import { createDeckService } from '../services/decksServices'
+
+export const createDeck = async (req: any, res: any) => {
+  const { topic, deckName, isPublic } = req.body
+  const { userId } = req.user
+  try {
+    await createDeckService(topic, deckName, isPublic, userId)
+    res.status(200).send({ message: 'Deck created' })
+  } catch (error) {
+    res.status(400).send({ message: 'Something went wrong', error })
+  }
 }
