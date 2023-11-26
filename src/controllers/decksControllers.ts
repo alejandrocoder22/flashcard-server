6d6a61380c541758d6a908374b0b206ded44d042
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-26 10:56:02 +0100

    Created skeletoon for desk petitions

---
+++ src/controllers/decksControllers.ts
@@ -0,0 +1,9 @@
+// Get decks by topic
+// Get decks by userId
+// Create Deck
+// Delete Deck
+// Update Deck
+
+const getDecksByTopic = (req, res) => {
+  c
+}
---
+++ src/router/decksRouter.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express'
+
+const router = Router()
+
+// @/api/decks
+router.get('')
