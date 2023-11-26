8aac866c8dc7e93d6ba8770ad5dca3caddf27f31
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-26 12:24:55 +0100

    Updated flashcard type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,9 @@
+
 export interface flashCard {
   card_id?: number
-  topic: string
   question: string
   answer: string
-  user_id?: number
-  is_public: boolean
+  deck_id?: number
   created_at?: string
   user_id?: number
 }
