176bdc91073839b675612153067df9ca251a51c8
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-11-28 08:50:13 +0100

    Added deckCardByDeckId router

--- src/router/decksRouter.ts
+++ src/router/decksRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createDeck, deleteDeck, getDecksByUserId, getPublicDecksByTopic, updateDeck } from '../controllers/decksControllers'
+import { createDeck, deleteDeck, getDeckAndCardsById, getDecksByUserId, getPublicDecksByTopic, updateDeck } from '../controllers/decksControllers'
 import { validateUser } from '../middlewares/validateUser'
 
 const router = Router()
 
 // @/api/decks
 router.post('/', validateUser, createDeck)
+router.get('/byId/:deckId', validateUser, getDeckAndCardsById)
 router.get('/', getPublicDecksByTopic)
 router.get('/:pagination', validateUser, getDecksByUserId)
 router.delete('/:deckId', validateUser, deleteDeck)
